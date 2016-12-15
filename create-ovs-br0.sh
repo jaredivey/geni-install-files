f7c6843b71ab88e2e2b12b442b60b64bd71e3bca
Jared Ivey <gth856t@mail.gatech.edu>
2016-12-15 13:50:09 -0500

    Add ovs-br0 script

---
+++ create-ovs-br0.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+ovs-vsctl add-br br0
+for intf in $(ifconfig -s)
+do
+    # Last interface should be controller so don't add to bridge
+    case $intf in
+    eth*)
+        ETHx=$intf
+    ;;
+    esac
+done
+for intf in $(ifconfig -s)
+do
+    case $intf in
+    eth0)
+        #echo "Skip $intf public interface"
+        ;;
+    $ETHx)
+        #echo "Skip $intf controller interface"
+        ;;
+    eth*)
+        #echo "Configure $intf"
+        ifconfig $intf 0
+        ovs-vsctl add-port br0 $intf
+    ;;
+    *)
+    ;;
+    esac
+done
+#ovs-vsctl list-ports br0
