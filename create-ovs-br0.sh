7399192e6bb3c78c465052949dd9989fa3e085b3
Jared Ivey <gth856t@mail.gatech.edu>
2016-12-18 18:08:10 -0500

    Update method for creating OVS bridge

--- create-ovs-br0.sh
+++ create-ovs-br0.sh
@@ -1,32 +1,29 @@
 #! /bin/bash
-sudo ovs-vsctl add-br br0
+#sudo ovs-vsctl add-br br0
+let 'eths=0'
 for intf in $(ifconfig -s)
 do
     # Last interface should be controller so don't add to bridge
     case $intf in
     eth*)
-        ETHx=$intf
+        let 'eths++'
     ;;
     esac
 done
-for intf in $(ifconfig -s)
+echo $eths
+for (( intf=0 ; intf<$eths ; intf++ ))
 do
-    case $intf in
-    eth0)
-        #echo "Skip $intf public interface"
-        ;;
-    $ETHx)
-        #echo "Skip $intf controller interface"
-        ;;
-    eth*)
-        #echo "Configure $intf"
-        sudo ifconfig $intf 0.0.0.0
-        sudo ovs-vsctl add-port br0 $intf
+    ethX=eth$intf
+    ipaddr=$(ifconfig $ethX | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}')
+    case $ipaddr in
+    10.10.1.*)
+        sudo ifconfig $ethX 0.0.0.0
+        sudo ovs-vsctl add-port br0 $ethX
     ;;
     *)
     ;;
     esac
 done
-sudo ovs-vsctl set-controller br0 tcp:11.1.1.1:6653
-sudo ovs-vsctl set-fail-mode br0 secure
+#sudo ovs-vsctl set-controller br0 tcp:11.1.1.1:6653
+#sudo ovs-vsctl set-fail-mode br0 secure
 #ovs-vsctl list-ports br0
