8fcfd889f9f52a94004b2cac182618f642cbf833
Jared Ivey <gth856t@mail.gatech.edu>
2016-12-18 17:18:12 -0500

    Add a few more deps

--- prep-and-run-ryu.sh
+++ prep-and-run-ryu.sh
@@ -7,8 +7,9 @@ if [ ! -f "./installed-ctrl-deps.txt" ]
 then
        #### Create the file ####
         sh -i -c `sudo touch "./installed-ctrl-deps.txt"`
-        sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install at python-dev python-pip python-greenlet python-greenlet-dev
-        sudo pip -q install eventlet routes webob paramiko oslo.config oslo.i18n netaddr rfc3986 repoze.lru tinyrpc
+        sudo apt-get update
+        sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install at python-dev build-essential python-pip python-greenlet python-greenlet-dev
+        sudo pip -q install eventlet routes webob paramiko babel debtcollector oslo.config oslo.i18n netaddr rfc3986 repoze.lru tinyrpc
         sudo git clone git://github.com/osrg/ryu.git /local/geni-install-files/ryu
         cd /local/geni-install-files/ryu
         sudo python ./setup.py install
