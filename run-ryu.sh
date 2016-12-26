3c471a620e775234a3a464810e6d948925d0340e
Jared Ivey <gth856t@mail.gatech.edu>
2016-12-26 12:30:50 -0500

    Update supported protocols and initial Ryu app

--- run-ryu.sh
+++ run-ryu.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /local/geni-install-files/ryu
-sudo PYTHONPATH=. ./bin/ryu-manager --verbose --ofp-listen-host=0.0.0.0 --ofp-tcp-listen-port=6653 ryu/app/simple_switch.py &> /local/geni-install-files/ryu.log
+sudo PYTHONPATH=. ./bin/ryu-manager --verbose --ofp-listen-host=0.0.0.0 --ofp-tcp-listen-port=6653 ryu/app/simple_switch_13.py &> /local/geni-install-files/ryu.log
