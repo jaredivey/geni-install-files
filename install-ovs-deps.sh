928fca98150ca85fdbddcc61481c96262c70a0d3
Jared Ivey <gth856t@mail.gatech.edu>
2016-12-29 12:07:49 -0500

    Update to install OVS from source with MPLS patch

--- install-ovs-deps.sh
+++ install-ovs-deps.sh
@@ -9,7 +9,24 @@ then
         sh -i -c `sudo touch "./installed-ovs-deps.txt"`
 
        #### Run  one-time commands ####
-        sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install at
+        sudo git clone https://github.com/openvswitch/ovs /local/ovs
+        cd /local/ovs
+        sudo patch -p1 -i /local/geni-install-files/ovs.patch
+        sudo ./boot.sh
+        sudo ./configure
+        sudo make
+        sudo make install
+        sudo /sbin/modprobe openvswitch
+
+        sudo mkdir -p /usr/local/etc/openvswitch
+        sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+        sudo mkdir -p /usr/local/var/run/openvswitch
+        sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+            --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+            --pidfile --detach --log-file
+        sudo ovs-vsctl --no-wait init
+        sudo ovs-vswitchd --pidfile --detach --log-file
+
         sudo chmod +x /local/geni-install-files/create-ovs-br0.sh
-        sudo at now +5 minutes -f /local/geni-install-files/create-ovs-br0.sh
+        sudo bash /local/geni-install-files/create-ovs-br0.sh $1
 fi
