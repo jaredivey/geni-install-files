d21ed2d52ea6995001a2f75ffe6ee385bf77ae4e
Jared Ivey <gth856t@mail.gatech.edu>
2017-01-08 14:57:35 -0500

    Patch Ryu for LLDP delay

--- prep-and-run-ryu.sh
+++ prep-and-run-ryu.sh
@@ -12,8 +12,12 @@ then
         sudo pip -q install eventlet routes webob paramiko babel debtcollector pytz pbr wrapt oslo.config oslo.i18n netaddr rfc3986 repoze.lru tinyrpc
         sudo git clone git://github.com/osrg/ryu.git /local/geni-install-files/ryu
         cd /local/geni-install-files/ryu
+        patch -p1 -i ../ryu.patch
         sudo python ./setup.py install
         cd /local/geni-install-files
+        sudo git clone git://github.com/jaredivey/dce-python-sdn /local/geni-install-files/dce-python-sdn
+        sudo ln -s /local/geni-install-files/dce-python-sdn/nix_simple.py /local/geni-install-files/ryu/ryu/app/nix_simple.py
+        sudo ln -s /local/geni-install-files/dce-python-sdn/nix_mpls_geni.py /local/geni-install-files/ryu/ryu/app/nix_mpls_geni.py
         sudo chmod +x /local/geni-install-files/run-ryu.sh
         sudo at now +1 minute -f /local/geni-install-files/run-ryu.sh
 fi
