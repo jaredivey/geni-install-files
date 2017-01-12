10be2dfd1162b5f7d68ef4783b947c6199959f3a
Jared Ivey <gth856t@mail.gatech.edu>
2017-01-12 14:55:46 -0500

    Use Python 2.7.13 to run Ryu

--- prep-and-run-ryu.sh
+++ prep-and-run-ryu.sh
@@ -8,7 +8,15 @@ then
        #### Create the file ####
         sh -i -c `sudo touch "./installed-ctrl-deps.txt"`
         sudo apt-get update
-        sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install at python-dev build-essential python-pip python-greenlet python-greenlet-dev
+        sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q install at
+        sudo hg clone https://hg.python.org/cpython -u v2.7.13
+        cd /local/cpython
+        sudo ./configure
+        sudo make
+        sudo make install
+        cd /local
+        sudo wget https://bootstrap.pypa.io/get-pip.py
+        sudo python get-pip.py
         sudo pip -q install eventlet routes webob paramiko babel debtcollector pytz pbr wrapt oslo.config oslo.i18n netaddr rfc3986 repoze.lru tinyrpc
         sudo git clone git://github.com/osrg/ryu.git /local/geni-install-files/ryu
         cd /local/geni-install-files/ryu
