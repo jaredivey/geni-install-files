9d78dcceeaad2cf52089317a8f0a6e3622ed5137
Jared Ivey <gth856t@mail.gatech.edu>
2017-01-20 18:12:25 -0500

    Hide OVS patching for now

--- install-ovs-deps.sh
+++ install-ovs-deps.sh
@@ -9,23 +9,23 @@ then
         sh -i -c `sudo touch "./installed-ovs-deps.txt"`
 
        #### Run  one-time commands ####
-        sudo git clone https://github.com/openvswitch/ovs /local/ovs
-        cd /local/ovs
-        sudo patch -p1 -i /local/geni-install-files/ovs.patch
-        sudo ./boot.sh
-        sudo ./configure
-        sudo make
-        sudo make install
-        sudo /sbin/modprobe openvswitch
+        #sudo git clone https://github.com/openvswitch/ovs /local/ovs
+        #cd /local/ovs
+        #sudo patch -p1 -i /local/geni-install-files/ovs.patch
+        #sudo ./boot.sh
+        #sudo ./configure
+        #sudo make
+        #sudo make install
+        #sudo /sbin/modprobe openvswitch
 
-        sudo mkdir -p /usr/local/etc/openvswitch
-        sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
-        sudo mkdir -p /usr/local/var/run/openvswitch
-        sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
-            --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
-            --pidfile --detach --log-file
-        sudo ovs-vsctl --no-wait init
-        sudo ovs-vswitchd --pidfile --detach --log-file
+        #sudo mkdir -p /usr/local/etc/openvswitch
+        #sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+        #sudo mkdir -p /usr/local/var/run/openvswitch
+        #sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+        #    --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+        #    --pidfile --detach --log-file
+        #sudo ovs-vsctl --no-wait init
+        #sudo ovs-vswitchd --pidfile --detach --log-file
 
         sudo chmod +x /local/geni-install-files/create-ovs-br0.sh
         sudo bash /local/geni-install-files/create-ovs-br0.sh $1
