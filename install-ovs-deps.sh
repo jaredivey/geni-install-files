00c9b429897db02341590c7bc38f248c5a896e22
Jared Ivey <gth856t@mail.gatech.edu>
2017-01-21 10:25:03 -0500

    Uncomment the OVS install

--- install-ovs-deps.sh
+++ install-ovs-deps.sh
@@ -9,25 +9,25 @@ then
         sh -i -c `sudo touch "./installed-ovs-deps.txt"`
 
        #### Run  one-time commands ####
-        #sudo git clone https://github.com/openvswitch/ovs /local/ovs
-        #cd /local/ovs
-        #sudo patch -p1 -i /local/geni-install-files/ovs.patch
-        #sudo ./boot.sh
-        #sudo ./configure
-        #sudo make
-        #sudo make install
-        #sudo /sbin/modprobe openvswitch
+        sudo git clone https://github.com/openvswitch/ovs /local/ovs
+        cd /local/ovs
+        sudo patch -p1 -i /local/geni-install-files/ovs.patch
+        sudo ./boot.sh
+        sudo ./configure
+        sudo make
+        sudo make install
+        sudo /sbin/modprobe openvswitch
 
-        #sudo mkdir -p /usr/local/etc/openvswitch
-        #sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
-        #sudo mkdir -p /usr/local/var/run/openvswitch
+        sudo mkdir -p /usr/local/etc/openvswitch
+        sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+        sudo mkdir -p /usr/local/var/run/openvswitch
 fi
 
-#sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
-#    --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
-#    --pidfile --detach --log-file
-#sudo ovs-vsctl --no-wait init
-#sudo ovs-vswitchd --pidfile --detach --log-file
+sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+    --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+    --pidfile --detach --log-file
+sudo ovs-vsctl --no-wait init
+sudo ovs-vswitchd --pidfile --detach --log-file
 
 if [ ! -f "./create-ovs-br0.txt" ]
 then
