c97428a7148d0bcab86e44b0cbc2e2a90f3d34bb
Jared Ivey <gth856t@mail.gatech.edu>
2017-01-21 10:21:16 -0500

    Comment out the iperf servers

--- install-iperf.sh
+++ install-iperf.sh
@@ -15,10 +15,10 @@ fi
 # Set iperf servers to listen on distinct ports for each host so the bind on the client side
 # will not interfere with already bound address/port combination
 sudo mkdir -p /local/iperfs
-for listener in {0..127}
-do
-        let port=45000+$listener
-        sudo echo "iperf -s -i1s -p$port -B10.10.$1.$2 > /users/jivey/iperfs-10.10.$1.$2.$port.log" > /local/iperfs/start-iperf-10.10.$1.$1.$port.sh
-        sudo chmod +x /local/iperfs/start-iperf-10.10.$1.$1.$port.sh
-        sudo at now -f /local/iperfs/start-iperf-10.10.$1.$1.$port.sh
-done
+#for listener in {0..127}
+#do
+#        let port=45000+$listener
+#        sudo echo "iperf -s -i1s -p$port -B10.10.$1.$2 > /users/jivey/iperfs-10.10.$1.$2.$port.log" > /local/iperfs/start-iperf-10.10.$1.$1.$port.sh
+#        sudo chmod +x /local/iperfs/start-iperf-10.10.$1.$1.$port.sh
+#        sudo at now -f /local/iperfs/start-iperf-10.10.$1.$1.$port.sh
+#done
